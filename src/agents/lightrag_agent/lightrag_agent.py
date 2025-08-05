d5bc812229103fde6d312bb4effee753d2219f98
mahdisabour <msabour.official@gmail.com>
2025-08-05 16:55:02 +0000

    feat: handle lighrag_agent messages

--- src/agents/lightrag_agent/lightrag_agent.py
+++ src/agents/lightrag_agent/lightrag_agent.py
@@ -1,27 +1,37 @@
 import asyncio
 
 from typing import TypedDict
-from langgraph.graph import StateGraph, END
+from langgraph.graph import END, MessagesState, StateGraph
+from langchain_core.messages import AIMessage, HumanMessage
 from .utils import RagSystem, WORKING_DIR
+
+
+class AgentState(MessagesState, total=False):
+    ...
 
 
 rag_system = RagSystem(WORKING_DIR)
 
-class GraphState(TypedDict):
-    message: str
 
-def retrieve_generation(state: GraphState) -> GraphState:
+def retrieve_generation(state: AgentState) -> AgentState:
     """A node that appends a string to the message in the state."""
     asyncio.run(rag_system.initialize())
-    question = "What is the main theme of the story?"
-    result = asyncio.run(rag_system.query(question, mode="hybrid", top_k=20, chunk_top_k=5))
-    return {"message": result}
+    result = asyncio.run(rag_system.query(state["messages"][-1].content, mode="hybrid", top_k=20, chunk_top_k=5))
+    messages = state["messages"]
+    ai_message = AIMessage(result)
+    return {"messages": messages + [ai_message]}
 
-workflow = StateGraph(GraphState)
+workflow = StateGraph(AgentState)
 
-workflow.add_node("node_1", retrieve_generation)
+workflow.add_node("retrieve_generation", retrieve_generation)
 
-workflow.set_entry_point("node_1")
-workflow.add_edge("node_1", END)
+workflow.set_entry_point("retrieve_generation")
+workflow.add_edge("retrieve_generation", END)
 
 lightrag_agent = workflow.compile()
+
+
+if __name__ == "__main__":
+    message = HumanMessage("What is the main theme of the story?")
+    result = lightrag_agent.invoke(input=MessagesState(messages=[message]))
+    print(result)
